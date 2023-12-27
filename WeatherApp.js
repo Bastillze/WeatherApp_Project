a6f313d146d8cfe88cc4e6ed1ab03b9b1d016b84
Bastillze <36552461+Bastillze@users.noreply.github.com>
2023-12-26 21:18:09 -0500

    Fourth Commit- Updated JS Fetch file & HTML

--- WeatherApp.js
+++ WeatherApp.js
@@ -1,9 +1,30 @@
+
+//Target main div//
+
+const main = document.getElementById("main");
+
+
 //Function to call location's weather info from API website//
 
 const fetchPromise = fetch("http://weatherapi.com", { mode: "cors" });
 
-fetchPromise.then((response) => {
-  console.log(response);
-});
+fetchPromise
+.then((response) => {
+  return response.json();
+})
+ .then((weather) => {
+  const location = location.map((area) => {
+    return location.area;
+  }).join("/n");
+ main.innerHTML= location;
+ });
 
 //Function that processess the JSON information from weather API website//
+
+function places(condition) {
+  const location = location.map((area) => {
+    return `<li>${location.area}</li>`
+  })
+  .join("/n");
+
+}
