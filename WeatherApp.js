02932eaebd94d79220a0cba5d79906c134304846
Bastillze <36552461+Bastillze@users.noreply.github.com>
2023-12-30 19:16:47 -0500

    Add files via upload
    Fifth Commit- Updated JS File

--- WeatherApp.js
+++ WeatherApp.js
@@ -1,30 +1,31 @@
-
 //Target main div//
 
 const main = document.getElementById("main");
 
+//Loading Placholder
+
+main.innerHTML = "<p>Loading...</p>";
 
 //Function to call location's weather info from API website//
 
 const fetchPromise = fetch("http://weatherapi.com", { mode: "cors" });
 
 fetchPromise
-.then((response) => {
-  return response.json();
-})
- .then((weather) => {
-  const location = location.map((area) => {
-    return location.area;
-  }).join("/n");
- main.innerHTML= location;
- });
+  .then((response) => {
+    return response.json();
+  })
+  .then((weather) => {
+    main.innerHTML = places(location);
+  });
 
 //Function that processess the JSON information from weather API website//
 
 function places(condition) {
-  const location = location.map((area) => {
-    return `<li>${location.area}</li>`
-  })
-  .join("/n");
+  const location = location
+    .map((area) => {
+      return `<li>${location.area}</li>`;
+    })
+    .join("/n");
 
+  return `<ul>${area}</ul>`;
 }
