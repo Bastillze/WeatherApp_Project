987a4c30517f259baa41e1df0aa62712c1b21974
Bastillze <36552461+Bastillze@users.noreply.github.com>
2024-01-15 21:31:27 -0500

    Sixth Commit
    Updated weather JS

--- WeatherApp.js
+++ WeatherApp.js
@@ -1,22 +1,25 @@
+//Function to call location's weather info from API website//
+const fetchPromise = fetch(
+  "http://api.weatherapi.com/v1/current.json?key=3c836a38db6d4e46b7950259231912&q=London&aqi=no",
+  { mode: "cors" }
+);
 
 //Target main div//
 
 const main = document.getElementById("main");
 
-
-//Function to call location's weather info from API website//
-
-const fetchPromise = fetch("http://api.weatherapi.com/v1/current.json?key=3c836a38db6d4e46b7950259231912&q=London&aqi=no", { mode: "cors" });
-
-fetchPromise
-.then((response) => {
-  return response.json();
-})
-.then((weather => {
-  var climate = climate.map((weather) => {
-    return climate.weather;
-  })
-  console.log(weather);
-}))
- 
 //Function that processess the JSON information from weather API website//
+fetchPromise
+
+  .then((response) => {
+    return response.json();
+  })
+  .then((weather) => {
+    const location = function (regionWeather) {
+      location.weather.join("\n");
+    };
+    console.log(weather);
+  });
+//Append weather info into main DIV element//
+
+main.innerHTML.HTML = location;
