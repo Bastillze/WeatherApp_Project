0d4bee1bbd2f24d6e26b07913fcdea65c3cc7c91
Bastillze <36552461+Bastillze@users.noreply.github.com>
2024-03-03 01:11:07 -0500

    Newest Commit
    Further updated JS fetch promise

--- WeatherApp.js
+++ WeatherApp.js
@@ -1,12 +1,13 @@
 //Function to call location's weather info from API website//
 function fetchData() {
-  fetch(
+  return fetch(
     "http://api.weatherapi.com/v1/current.json?key=3c836a38db6d4e46b7950259231912&q=London&aqi=no"
-  )
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+  ).then((response) => response.json());
 }
-
-fetchData();
-
-//Display the code in the HTML//
+const data = () => {
+  Promise.then(
+    (data) => {},
+    (error) => {}
+  );
+};
+console.log(fetchData);
