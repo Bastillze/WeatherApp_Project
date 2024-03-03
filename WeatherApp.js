b010ed05a82893cfc8275fa3cc056cdb34e79689
Bastillze <36552461+Bastillze@users.noreply.github.com>
2024-03-03 00:20:37 -0500

    Newest Commit
    Updated Weather app JavaScript, altered fetch promise

--- WeatherApp.js
+++ WeatherApp.js
@@ -4,13 +4,9 @@ function fetchData() {
     "http://api.weatherapi.com/v1/current.json?key=3c836a38db6d4e46b7950259231912&q=London&aqi=no"
   )
     .then((response) => response.json())
-    .then((document) => {
-      return WebGL2RenderingContext(document);
-    })
-    .then((rendered) => {
-      cacheInDatabase(rendered);
-    })
-    .catch((error) => error);
+    .then((data) => console.log(data));
 }
+
+fetchData();
 
 //Display the code in the HTML//
