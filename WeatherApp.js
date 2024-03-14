d1f089ed980fd2c842d50fcea435caf872b5883c
Bastillze <36552461+Bastillze@users.noreply.github.com>
2024-03-13 22:09:46 -0400

    Newest Commit(3/13/24)
    Updated JavaScript and CSS

--- WeatherApp.js
+++ WeatherApp.js
@@ -4,7 +4,10 @@ function fetchData() {
     "http://api.weatherapi.com/v1/current.json?key=3c836a38db6d4e46b7950259231912&q=London&aqi=no"
   )
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => console.log(data))
+    .then((data) => {
+      document.getElementById("main").textContent = JSON.stringify;
+    });
 }
 
 fetchData();
