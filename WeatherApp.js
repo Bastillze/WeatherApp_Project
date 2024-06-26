ba2632696569a4c3d1dc9d7d2cfcb1b04885f85e
Bastillze <36552461+Bastillze@users.noreply.github.com>
2024-06-26 01:40:58 -0400

    Updated async function+
    Updated async function and if/else/try/catch statements.

--- WeatherApp.js
+++ WeatherApp.js
@@ -7,7 +7,7 @@ const card = document.querySelector(".card");
 const apiKey = "3c836a38db6d4e46b7950259231912";
 
 
-weatherForm.addEventListener("submit", event => {
+weatherForm.addEventListener("submit", async event => {
 
     event.preventDefault();
 
@@ -15,15 +15,29 @@ weatherForm.addEventListener("submit", event => {
 
     if(city){
 
+        try{
+             const weatherData = await getWeatherData(city);
+             displayWeatherInfo(weatherData);
+        }
+         catch(error){
+               console.error(error);
+               displayError(error);
+         }
     }
     else{
-      
+           displayError("Please enter a city");
     }
 
 
 });
 
 async function getWeatherData(city){
+
+    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=3c836a38db6d4e46b7950259231912&q=city&aqi=yes${apiKey}`;
+
+    const response = await fetch(apiUrl);
+
+    console.log(response);
 
 }
 
@@ -36,5 +50,13 @@ function getWeatherEmoji(weatherId){
 }
 
 function displayError(message){
+
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
 
 }
